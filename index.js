0e3e7c192ff9607c68f7ab8f406a6205a50f7faa
tarekwagih43 <tarekwagih43@gmail.com>
2021-03-18 03:46:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
-const mongoose = require('mongoose');
-
-const Post = require('./models/Post');
-const { MONGODB } = require('./config.js');
 
 console.log('Hello');
